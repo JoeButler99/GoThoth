d7a7b10467d94cea640fba94c89953768d3a3793
Joe Butler <joe.butler@hsbc.com>
2017-12-05 16:44:47 +0000

    Adding the start of the function tests

--- rpn_actions.go
+++ rpn_actions.go
@@ -23,3 +23,19 @@ func RPNSquare(rpnStack *[50]float64, highest *uint) {
 	*highest --
 	rpnStack[*highest] = rpnStack[*highest] * rpnStack[*highest]
 }
+
+
+func RPNDivide(rpnStack *[50]float64, highest *uint) {
+	*highest -= 2
+	if rpnStack[*highest] == 0.0 {
+		rpnStack[*highest] = rpnStack[*highest+1]
+	} else {
+		rpnStack[*highest] = rpnStack[*highest+1] / rpnStack[*highest]
+	}
+
+}
+
+func RPNPercentMe(rpnStack *[50]float64, highest *uint) {
+	*highest --
+	rpnStack[*highest] = rpnStack[*highest] / 100
+}
