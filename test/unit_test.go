4bd0b70dc731e46073c6c67a754c5a12d8d0bc9a
Joe Butler <joe.butler@hsbc.com>
2017-12-05 16:44:41 +0000

    Adding the start of the function tests

--- test/unit_test.go
+++ test/unit_test.go
@@ -1,10 +1,10 @@
 package Test
 
 import (
-	"testing"
 	"Thoth"
+	"fmt"
+	"testing"
 )
-
 
 func TestRPNAdd(t *testing.T) {
 	var stack = [50]float64{}
@@ -47,6 +47,55 @@ func TestRPNSquare(t *testing.T) {
 	var highest uint = 1
 	Thoth.RPNSquare(&stack, &highest)
 	if stack[0] != 16 {
+		t.Fail()
+	}
+}
+
+func TestPRNDivide(t *testing.T) {
+	var stack = [50]float64{}
+	stack[0] = 4
+	stack[1] = 8
+	var highest uint = 2
+	Thoth.RPNDivide(&stack, &highest)
+	if stack[0] != 2 {
+		fmt.Println("stack 0 is %d", stack[0])
+		t.Fail()
+
+	}
+}
+
+func TestRPNPercentMe(t *testing.T) {
+	var stack = [50]float64{}
+	stack[0] = 50
+	var highest uint = 1
+	Thoth.RPNPercentMe(&stack, &highest)
+	if stack[0] != 0.5 {
+		t.Fail()
+	}
+}
+
+func TestFunction(t *testing.T) {
+	f := Thoth.Function{
+		Name:      "Fadd",
+		Inputs:    2,
+		RPNAction: Thoth.RPNAdd,
+	}
+
+	var stack = [50]float64{}
+	var highest uint = 2
+	stack[0] = 1
+	stack[1] = 2
+
+	f.RPNAction(&stack, &highest)
+
+	if stack[0] != 3 {
+		t.Fail()
+	}
+
+	f.RPNAction = Thoth.RPNSquare
+	highest = 1
+	f.RPNAction(&stack, &highest)
+	if stack[0] != 9 {
 		t.Fail()
 	}
 }
