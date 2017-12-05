c143fada4fe51d228ba0535faa516726f3b72e6d
Joe Butler <joe.butler@hsbc.com>
2017-12-05 16:55:45 +0000

    created an 'assert' function for checking a stack element

--- test/unit_test.go
+++ test/unit_test.go
@@ -6,6 +6,13 @@ import (
 	"testing"
 )
 
+func assert_stack_element(t *testing.T, rpnStack *[50]float64, element uint, expected float64) {
+	if rpnStack[element] != expected {
+		fmt.Printf("Expected %f got %f\n", expected, rpnStack[element])
+		t.Fail()
+	}
+}
+
 func TestRPNAdd(t *testing.T) {
 	var stack = [50]float64{}
 	stack[0] = 2
@@ -23,9 +30,7 @@ func TestRPNSubtract(t *testing.T) {
 	stack[1] = 7
 	var highest uint = 2
 	Thoth.RPNSubtract(&stack, &highest)
-	if stack[0] != 5 {
-		t.Fail()
-	}
+	assert_stack_element(t, &stack, 0, 5)
 
 }
 
@@ -35,9 +40,7 @@ func TestRPNMultiply(t *testing.T) {
 	stack[1] = 7
 	var highest uint = 2
 	Thoth.RPNMultiply(&stack, &highest)
-	if stack[0] != 14 {
-		t.Fail()
-	}
+	assert_stack_element(t, &stack, 0, 14)
 
 }
 
@@ -46,9 +49,7 @@ func TestRPNSquare(t *testing.T) {
 	stack[0] = 4
 	var highest uint = 1
 	Thoth.RPNSquare(&stack, &highest)
-	if stack[0] != 16 {
-		t.Fail()
-	}
+	assert_stack_element(t, &stack, 0, 16)
 }
 
 func TestPRNDivide(t *testing.T) {
@@ -57,11 +58,7 @@ func TestPRNDivide(t *testing.T) {
 	stack[1] = 8
 	var highest uint = 2
 	Thoth.RPNDivide(&stack, &highest)
-	if stack[0] != 2 {
-		fmt.Println("stack 0 is %d", stack[0])
-		t.Fail()
-
-	}
+	assert_stack_element(t, &stack, 0, 2)
 }
 
 func TestRPNPercentMe(t *testing.T) {
@@ -69,9 +66,7 @@ func TestRPNPercentMe(t *testing.T) {
 	stack[0] = 50
 	var highest uint = 1
 	Thoth.RPNPercentMe(&stack, &highest)
-	if stack[0] != 0.5 {
-		t.Fail()
-	}
+	assert_stack_element(t, &stack, 0, 0.5)
 }
 
 func TestFunction(t *testing.T) {
@@ -88,14 +83,10 @@ func TestFunction(t *testing.T) {
 
 	f.RPNAction(&stack, &highest)
 
-	if stack[0] != 3 {
-		t.Fail()
-	}
+	assert_stack_element(t, &stack, 0, 3)
 
 	f.RPNAction = Thoth.RPNSquare
 	highest = 1
 	f.RPNAction(&stack, &highest)
-	if stack[0] != 9 {
-		t.Fail()
-	}
+	assert_stack_element(t, &stack, 0, 9)
 }
